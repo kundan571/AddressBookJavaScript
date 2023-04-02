639959f4a9e17841be69646050e330dbe04f3b4e
kundan kumar <kundanchauhan1997@gmail.com>
2023-04-03 01:53:00 +0530

    Added: Ability to sort the entries in the address book alphabetically by Person’s name

--- AddressBook.js
+++ AddressBook.js
@@ -184,3 +184,6 @@ console.log("City Count: " + countInCity);
 let contactsInState = addressBookMainArray.filter(contact => contact.state === "karnatak");
 let countByState = contactsInState.length;
 console.log("State Count: " + countByState);
+// sort by name
+addressBookMainArray.sort((a, b) => a.firstName.localeCompare(b.firstName));
+console.log(addressBookMainArray);
